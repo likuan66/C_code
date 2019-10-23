cf6450f00bf74724fc3a588b93c82f6a98392cc0
likuan66 <1402424218@qq.com>
2019-10-23 17:02:11 +0800

    practice

---
+++ 课件代码/函数声明定义/test.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include<stdio.h>
+#include<string.h>
+#include<Windows.h>
+#pragma warning(disable:4996)
+
+int MyAdd(int x, int y);
+int MySub(int x, int y);
+int MyMul(int x, int y);
+int MyDiv(int x, int y);
+int MyMod(int x, int y);
